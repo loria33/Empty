42d03c8d9802db337f7bdf9a77ebab7831c2ef27
Lori Azerrad <lazerrad@H000879.local>
2020-11-23 15:18:32 -0500

    xamirin

---
+++ xamirin/RNTest/RNTest/EmptyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.UITest.Utils;
+
+namespace RNTest
+{
+    public class WaitTimes : IWaitTimes
+    {
+        public TimeSpan GestureWaitTimeout
+        {
+            get { return TimeSpan.FromMinutes(5); }
+        }
+
+        public TimeSpan WaitForTimeout
+        {
+            get { return TimeSpan.FromMinutes(5); }
+        }
+
+        public TimeSpan GestureCompletionTimeout => throw new NotImplementedException();
+    }
+}
