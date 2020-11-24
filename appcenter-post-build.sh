06c30bb4c648f7c7f5f7636c8602041e7d45640d
Lori Azerrad <lazerrad@H000879.local>
2020-11-23 19:07:58 -0500

    init

---
+++ appcenter-post-build.sh
@@ -0,0 +1,43 @@
+
+#!/usr/bin/env bash
+if [ "$APPCENTER_XAMARIN_CONFIGURATION" == "Debug" ];then
+
+    echo "--token $emptyApi"
+    echo "Post Build Script Started"
+
+    SolutionFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name RNTest.sln`
+    SolutionFileFolder=`dirname $SolutionFile`
+
+    UITestProject=`find "$APPCENTER_SOURCE_DIRECTORY" -name RNTest.csproj`
+
+    echo SolutionFile: $SolutionFile
+    echo SolutionFileFolder: $SolutionFileFolder
+    echo UITestProject: $UITestProject
+
+    chmod -R 777 $SolutionFileFolder
+
+    msbuild "$UITestProject" /property:Configuration=$APPCENTER_XAMARIN_CONFIGURATION
+
+    UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "RNTest.dll" | grep bin | head -1` 
+    echo UITestDLL: $UITestDLL
+
+    UITestBuildDir=`dirname $UITestDLL`
+    echo UITestBuildDir: $UITestBuildDir
+
+    UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
+    echo UITestVersionNumber: $UITestVersionNumber
+
+    TestCloudExe=`find ~/.nuget | grep test-cloud.exe | grep $UITestVersionNumber | head -1`
+    echo TestCloudExe: $TestCloudExe
+
+    TestCloudExeDirectory=`dirname $TestCloudExe`
+    echo TestCloudExeDirectory: $TestCloudExeDirectory
+
+    APKFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.apk | head -1`
+
+    npm install -g appcenter-cli
+
+    appcenter login --token token
+
+    appcenter test run uitest --app "lori.azerrad-curiositystream.com/TEST" --devices "3f03d1be" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --uitest-tools-dir $TestCloudExeDirectory --async
+fi
