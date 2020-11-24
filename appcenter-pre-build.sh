f64f46c3ab84ee1993cc3d4827bf73513bd1a903
Lori Azerrad <lazerrad@H000879.local>
2020-11-23 23:55:48 -0500

    dssdsdsd

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,6 +3,10 @@ PostBuildScriptFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name appcenter-post-bui
 echo PostBuildScriptFile
 echo $APPCENTER_ACCESS_TOKEN
 echo "THIS IS THE TOKEN ABOVE ME.."
+echo $loginToken
+
+echo "THIS IS THE loginToken TOKEN ABOVE ME.."
+
 sed -i '' "s/--token token/--token $APPCENTER_ACCESS_TOKEN/g" "$PostBuildScriptFile"
 
 echo "Finished Injecting App Center Login Token"
